275f6e94e129d51957c2befd53e486fa7c26df79
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-01-24 15:14:01 +0300

    CQRS GetAboutQueryHandler

---
+++ Core/CarBook.Application/Features/CQRS/Results/AboutResults/GetAboutByIdQueryResult.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CarBook.Application.Features.CQRS.Results.AboutResults
+{
+    public class GetAboutByIdQueryResult
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
